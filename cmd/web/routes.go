e838891be226ad8eaf32db0c5cb386c3c789d7a8
codermohit <iamthecodermohit@gmail.com>
2024-07-23 11:37:28 +0530

    feat : middleware added

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,7 +2,7 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux{
+func (app *application) routes() http.Handler{
 	mux := http.NewServeMux()
 
 	//file server to serve files from the "./ui/static" directory.
@@ -16,5 +16,5 @@ func (app *application) routes() *http.ServeMux{
 	//Create the new route, which is restricted to POST requests only
 	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
-  return mux
+  return commonHeaders(mux)
 }
