484c344e6e2ce0df48d90894a3dcc4b4f43354b9
codermohit <iamthecodermohit@gmail.com>
2024-07-26 14:05:54 +0530

    add : users model for authentication

--- internal/models/users.go
+++ internal/models/users.go
@@ -2,7 +2,12 @@ package models
 
 import (
 	"database/sql"
+	"errors"
+	"strings"
 	"time"
+
+	"github.com/go-sql-driver/mysql"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
@@ -13,23 +18,62 @@ type User struct {
 	Created        time.Time
 }
 
-
-//database connection pool
-type UserModel struct{
-  DB *sql.DB
+// database connection pool
+type UserModel struct {
+	DB *sql.DB
 }
 
+func (m *UserModel) Insert(name, email, password string) error {
+	//Create a bcrypt hash of the plain-text password
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err != nil {
+		return err
+	}
 
-func (u *UserModel) Insert(name, email, password string) error {
-  return nil
+	stmt := `INSERT INTO users (name, email, hashed_password, created)
+  VALUES(?,?,?,UTC_TIMESTAMP())`
+
+	//Exec() method to insert the user details and hashed password into users table
+	_, err = m.DB.Exec(stmt, name, email, string(hashedPassword))
+	if err != nil {
+		var mySLQError *mysql.MySQLError
+		if errors.As(err, &mySLQError) {
+			if mySLQError.Number == 1062 && strings.Contains(mySLQError.Message, "users_uc_email") {
+				return ErrDuplicateEmail
+			}
+		}
+		return err
+	}
+	return nil
 }
 
+func (m *UserModel) Authenticate(email, password string) (int, error) {
+	var id int
+	var hashedPassword []byte
 
-func (m *UserModel) Authenticate(email, password string)(int, error) {
-  return 0, nil
+	stmt := "SELECT id, hashed_password FROM users WHERE email = ?"
+
+	err := m.DB.QueryRow(stmt, email).Scan(&id, &hashedPassword)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return 0, ErrInvalidCredentials
+		} else {
+			return 0, err
+		}
+	}
+
+	//compare hash and password
+	err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return 0, ErrInvalidCredentials
+		} else {
+			return 0, err
+		}
+	}
+	return id, nil
 }
-
 
 func (m *UserModel) Exists(id int) (bool, error) {
-  return false, nil
+	return false, nil
 }
